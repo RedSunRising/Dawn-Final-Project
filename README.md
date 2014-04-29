9f94678cf11e00c2b02f3b58d1391b2b84666701
Dawn <dawn_passerini@harvard.edu>
2014-04-28 23:17:46 -0400

    Fixing the project
    Fixing the project.  Had to reload the entire thing

---
+++ README.md
@@ -0,0 +1,8 @@
+Dawn-Final-Project
+==================
+
+Final Project ideas/plans/build 
+
+I'd like to build a pedal type thing that would take audio and make visualizations that would interface with a projector.  I'd like to use stereo audio from a board mix.  Ideally, I'd like to use a 1/4" plug from a guitar or keyboard.
+
+I have to figuire out how to capture the frequencies, assign a color to a frequency range and randomized shapes.  I'd like to fade between the colors  and blend them.  I'd like the fade to be tied to the tempo of the music.  Also, I'd like to have the color intensity be tied to the relative loudness of audio being sent through.
---
+++ smartjava/webaudio/pDrawing.js
@@ -0,0 +1,54 @@
+    if (! window.AudioContext) {
+        if (! window.webkitAudioContext) {
+            alert('no audiocontext found');
+        }
+        window.AudioContext = window.webkitAudioContext;
+    }
+
+
+var context = new AudioContext();
+var javascriptNode;
+javascriptNode = context.createScriptProcessor(2048, 1, 1);
+
+
+    // get the context from the canvas to draw on
+    var c = document.getElementById('myCanvas');
+    var ctx = c.getContext("2d");
+
+    // create a gradient for the fill. Note the strange
+    // offset, since the gradient is calculated based on
+    // the canvas, not the specific element we draw
+    var gradient = ctx.createLinearGradient(0,0,0,130);
+    gradient.addColorStop(1,'#000000');
+    gradient.addColorStop(0.75,'#ff0000');
+    gradient.addColorStop(0.25,'#ffff00');
+    gradient.addColorStop(0,'#ffffff');
+
+
+
+
+    // when the javascript node is called
+    // we use information from the analyzer node
+    // to draw the volume
+    javascriptNode.onaudioprocess = function() {
+
+        // get the average for the first channel
+        var array =  new Uint8Array(analyser.frequencyBinCount);
+        analyser.getByteFrequencyData(array);
+        var average = getAverageVolume(array);
+
+        // get the average for the second channel
+        var array2 =  new Uint8Array(analyser2.frequencyBinCount);
+        analyser2.getByteFrequencyData(array2);
+        var average2 = getAverageVolume(array2);
+
+        // clear the current state
+        ctx.clearRect(0, 0, 60, 130);
+
+        // set the fill style
+        ctx.fillStyle=gradient;
+
+        // create the meters
+        ctx.fillRect(0,130-average,25,130);
+        ctx.fillRect(30,130-average2,25,130);
+    }
