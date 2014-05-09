073aabf390ccef2b9f78e36b6f9abe48b45b917c
labuser <labuser@IvyMac06.local>
2014-05-08 22:06:06 -0400

    flipped the bar
    We got the bar to flip!

--- smartjava/webaudio/pDrawing.js
+++ smartjava/webaudio/pDrawing.js
@@ -18,13 +18,25 @@
     function setup(){    
         p.size(300, 300);
         p.background(0);
-        p.rotate(180);
 
+    }
+
+    function draw() {
+        p.background(0);
+        p.translate(0, 300);
+        p.scale(1, -1);
+        drawRect1(average);
+        drawRect2(average2);
     }
 
   /*  function clearRect(){
         p.rect(0, 0, 60, 130);
     }*/
+
+    function square() {
+        p.fill(0, 255, 0);
+        p.rect(0,0, 100, 100);
+    }
 
     function drawRect1(average){
         p.fill(255, 0, 0);
@@ -32,30 +44,31 @@
     }
 
     function drawRect2(average2){
+
         p.fill(255, 255, 255);
         p.rect(30, 0, 25, 2*average2);
     }
 
+    var average = null;
+    var average2 = null;
     // when the javascript node is called
     // we use information from the analyzer node
     // to draw the volume
     javascriptNode.onaudioprocess = function() {
-
         // get the average for the first channel
         //clearRect();
-        p.background(0);
 
         var array =  new Uint8Array(analyser.frequencyBinCount);
         analyser.getByteFrequencyData(array);
-        var average = getAverageVolume(array);
+        average = getAverageVolume(array);
 
-        drawRect1(average);
+        // drawRect1(average);
 
         var array2 =  new Uint8Array(analyser2.frequencyBinCount);
         analyser2.getByteFrequencyData(array2);
-        var average2 = getAverageVolume(array2);
+        average2 = getAverageVolume(array2);
 
-        drawRect2(average2);
+        // drawRect2(average2);
         
         // clear the current state
        // ctx.clearRect(0, 0, 60, 130);
@@ -69,6 +82,7 @@
     }
 
     p.setup = setup;
+    p.draw = draw;
 }
 
 var p = new Processing(canvas, sketch);
